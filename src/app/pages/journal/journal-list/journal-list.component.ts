6ece4ecf92540e49189124138c9c98ae98025d3d
rxc-team <pit-support@proship.co.jp>
2025-02-10 16:40:59 +0900

    2025.02.10

--- src/app/pages/journal/journal-list/journal-list.component.ts
+++ src/app/pages/journal/journal-list/journal-list.component.ts
@@ -1,6 +1,5 @@
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
-import { NzResizeEvent } from 'ng-zorro-antd/resizable';
 
 import { Component, OnInit } from '@angular/core';
 import { AppService, JournalService } from '@api';
@@ -21,109 +20,71 @@ export class JournalListComponent implements OnInit {
     private tokenService: TokenStorageService
   ) {}
 
-  cols = [
-    {
-      title: 'common.text.no',
-      width: '40px'
-    },
-    {
-      title: 'page.journal.type',
-      width: '120px'
-    },
-    {
-      title: 'page.journal.debt',
-      width: '20%'
-    },
-    {
-      title: 'page.journal.credit',
-      width: '30%'
-    },
-    {
-      title: 'page.journal.amount',
-      width: '50%'
-    }
-  ];
-
   journals = [];
-
+  journalIds = [];
   confirmModal: NzModalRef;
 
   ngOnInit(): void {
-    this.js.getJournals().then((data: any[]) => {
+    this.js.getSelectJournals().then((data: any[]) => {
       if (data) {
-        this.journals = this.buildData(data);
+        this.journals = data;
+        this.journalIds = this.journals.map(journal => journal.journal_id);
       } else {
         this.journals = [];
       }
     });
   }
 
-  buildData(data: any[]) {
-    const dataList = [];
-    let line = 1;
-    data.forEach((journal, i) => {
-      const patterns: any[] = journal.patterns;
-      patterns.forEach((pattern, j) => {
-        const subjects: any[] = pattern.subjects;
-        dataList.push({
-          no: line,
-          name: pattern.pattern_name,
-          debt: '',
-          credit: '',
-          amount: '',
-          other: ''
-        });
-        subjects.forEach((subject, k) => {
-          dataList.push({
-            no: '',
-            name: `${line}-${k + 1}`,
-            debt: subject.lending_division === '2' ? subject.default_name : '',
-            credit: subject.lending_division === '1' ? subject.default_name : '',
-            amount: subject.amount_name,
-            other: ''
-          });
-        });
-        line++;
-      });
-    });
-    return dataList;
-  }
-
   async compute(section?: string) {
     const us = this.tokenService.getUser();
     const currentApp = us.current_app;
     const db = us.customer_id;
-    await this.appService.getAppByID(currentApp, db).then(res => {
-      if (res.swk_control) {
-        let confirmTitle = this.i18n.translateLang('common.message.confirm.journalTitle');
-        let confirmContent = this.i18n.translateLang('common.message.confirm.journalContent');
-        if (section === 'repay') {
-          confirmTitle = this.i18n.translateLang('common.message.confirm.repayJournalTitle');
-          confirmContent = this.i18n.translateLang('common.message.confirm.repayJournalContent');
-        }
-        if (section === 'pay') {
-          confirmTitle = this.i18n.translateLang('common.message.confirm.payJournalTitle');
-          confirmContent = this.i18n.translateLang('common.message.confirm.payJournalContent');
-        }
 
-        this.confirmModal = this.modal.confirm({
-          nzTitle: confirmTitle,
-          nzContent: confirmContent,
-          nzOnOk: async () =>
-            await this.js.computeJournal(section).then(data => {
-              this.message.success(this.i18n.translateLang('common.message.success.S_013'));
-            })
-        });
+    // 判断 this.journalIds 是否包含分录结构体
+    const isValidJournal = (section?: string): boolean => {
+      if (section === 'repay') {
+        return this.journalIds.includes('02');
+      } else if (section === 'pay') {
+        return this.journalIds.includes('03');
       } else {
-        this.message.error(this.i18n.translateLang('common.message.error.E_021'));
+        return this.journalIds.some(id => ['01', '04', '05', '06', '07'].includes(id));
       }
-    });
-  }
+    };
+    if (!isValidJournal(section)) {
+      if (section === 'repay') {
+        this.message.error(this.i18n.translateLang('common.message.error.E_026'));
+      } else if (section === 'pay') {
+        this.message.error(this.i18n.translateLang('common.message.error.E_025'));
+      } else {
+        this.message.error(this.i18n.translateLang('common.message.error.E_027'));
+      }
+      return;
+    } else {
+      await this.appService.getAppByID(currentApp, db).then(res => {
+        if (res.swk_control) {
+          let confirmTitle = this.i18n.translateLang('common.message.confirm.journalTitle');
+          let confirmContent = this.i18n.translateLang('common.message.confirm.journalContent');
+          if (section === 'repay') {
+            confirmTitle = this.i18n.translateLang('common.message.confirm.repayJournalTitle');
+            confirmContent = this.i18n.translateLang('common.message.confirm.repayJournalContent');
+          }
+          if (section === 'pay') {
+            confirmTitle = this.i18n.translateLang('common.message.confirm.payJournalTitle');
+            confirmContent = this.i18n.translateLang('common.message.confirm.payJournalContent');
+          }
 
-  /**
-   * @description: 调整表格行宽
-   */
-  onResize({ width }: NzResizeEvent, col: string): void {
-    this.cols = this.cols.map(e => (e.title === col ? { ...e, width: `${width}px` } : e));
+          this.confirmModal = this.modal.confirm({
+            nzTitle: confirmTitle,
+            nzContent: confirmContent,
+            nzOnOk: async () =>
+              await this.js.computeJournal(section).then(() => {
+                this.message.success(this.i18n.translateLang('common.message.success.S_013'));
+              })
+          });
+        } else {
+          this.message.error(this.i18n.translateLang('common.message.error.E_021'));
+        }
+      });
+    }
   }
 }
